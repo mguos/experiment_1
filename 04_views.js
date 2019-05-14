c8dedd45371ad6ccb9f93e8d1fde6f58ebcc8cc2
antoniabecker <49253023+antoniabecker@users.noreply.github.com>
2019-05-14 05:23:57 -0700

    changed name from key_press to keyPress

--- 04_views.js
+++ 04_views.js
@@ -102,7 +102,7 @@ const thanks = babeViews.thanks({
 
 
 // Here, we initialize a forcedChoice view
-const key_press_2A = babeViews.key_press({
+const key_press_2A = babeViews.keyPress({
     // This will use all trials specified in `data`, you can user a smaller value (for testing), but not a larger value
     trials: trial_info.key_press.length,
     // name and trial_type should be identical to the variable name
