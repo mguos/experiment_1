8cbfb49653206446f4890541b74904edc6e8c654
mguos <49288254+mguos@users.noreply.github.com>
2019-05-15 14:51:31 +0200

    Update 05_main.js

--- 05_main.js
+++ 05_main.js
@@ -23,7 +23,7 @@ $("document").ready(function() {
         ],
         // Here, you can specify all information for the deployment
         deploy: {
-            experimentID: "INSERT_A_NUMBER123",
+            experimentID: "INSERT_A_NUMBER",
             serverAppURL: "https://babe-demo.herokuapp.com/api/submit_experiment/",
             // Possible deployment methods are:
             // "debug" and "directLink"
