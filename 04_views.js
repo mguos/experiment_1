6b4f42e612582fe55544bae1760e8b9249f761fe
antoniabecker <49253023+antoniabecker@users.noreply.github.com>
2019-05-15 04:01:45 -0700

    Update 04_views.js

--- 04_views.js
+++ 04_views.js
@@ -102,12 +102,12 @@ const thanks = babeViews.thanks({
 
 
 // Here, we initialize a forcedChoice view
-const forced_choice_2A = babeViews.forcedChoice({
+const forced_choice_2A = babeViews.keyPress({
     // This will use all trials specified in `data`, you can user a smaller value (for testing), but not a larger value
     trials: trial_info.forced_choice.length,
     // name and trial_type should be identical to the variable name
-    name: 'forced_choice_2A',
-    trial_type: 'forced_choice_2A',
+    name: 'keyPress',
+    trial_type: 'keyPress',
     data: trial_info.forced_choice
 });
 
