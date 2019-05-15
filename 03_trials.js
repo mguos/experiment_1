e31c0ddab4d4281d9beebf1f1282a022f26b14d0
antoniabecker <49253023+antoniabecker@users.noreply.github.com>
2019-05-15 05:17:34 -0700

    Update 03_trials.js

--- 03_trials.js
+++ 03_trials.js
@@ -20,6 +20,33 @@ const trial_info = {
             y: 'same',
             m: 'different',
             expected: 'same'
+         },
+         {
+            question: "Is this the same object?",
+            picture: "images/1_50_different.jpg",
+            key1: 'y',
+            key2: 'm'
+            y: 'same',
+            m: 'different',
+            expected: 'different'
+        },
+         {
+            question: "Is this the same object?",
+            picture: "images/1_50_same.jpg",
+            key1: 'y',
+            key2: 'm'
+            y: 'same',
+            m: 'different',
+            expected: 'same'
+        },
+         {
+            question: "Is this the same object?",
+            picture: "images/2_50_different.jpg",
+            key1: 'y',
+            key2: 'm'
+            y: 'same',
+            m: 'different',
+            expected: 'different'
         }
     ]
 };
